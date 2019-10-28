096e0f2d20c93041602101d2426445ca79bc99b5
booobi <qqwweee59@gmail.com>
2019-10-28 18:12:18 +0200

    Add basic task creation component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -10,16 +10,19 @@ import { AppComponent } from './app.component';
 import { TaskListComponent } from './tasks/task-list/task-list.component';
 import { TaskListReducer } from './tasks/task-list/store/task-list.reducer';
 import { TaskListEffects } from './tasks/task-list/store/task-list.effects';
+import { TaskNewComponent } from './tasks/task-new/task-new.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'tasks' },
-  { path: 'tasks', component: TaskListComponent }
+  { path: 'tasks', component: TaskListComponent },
+  { path: 'tasks/new', component: TaskNewComponent }
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
-    TaskListComponent
+    TaskListComponent,
+    TaskNewComponent
   ],
   imports: [
     BrowserModule,
